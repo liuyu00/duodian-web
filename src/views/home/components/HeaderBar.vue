8bcdd978e3724dd218c6bb56ef67ed81d599c844
liuyu00 <liuyu009x@163.com>
2022-09-20 17:00:53 +0800

    第一次关联远程仓库的初始化工作

---
+++ src/views/home/components/HeaderBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header-bar">
+    <a href="javascript:;">
+      <i class="iconfont icon-dizhiguanli"></i>
+      <output>送至：北京八维研修学院</output>
+      <i class="iconfont icon-jiantouyou"></i>
+    </a>
+    <a href="javascript:;">
+      <i class="iconfont icon-sousuo"></i>
+    </a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header-bar'
+}
+</script>
+
+<style lang="scss" scoped>
+.header-bar {
+  @include wh(100%, 98px);
+  @include flex(row, center, space-between);
+  background: #fff;
+  padding: 0 20px;
+  box-sizing: border-box;
+  a {
+    color: $color-text-grey;
+    font-size: 24px;
+    output {
+      padding: 0 10px;
+    }
+    i {
+      font-size: 28px;
+    }
+  }
+}
+</style>
---
+++ src/views/main/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="page">
+    <router-view></router-view>
+    <nav class="footer-nav">
+      <router-link
+        v-for="(item, index) in tabs"
+        :key="index"
+        :to="item.path"
+        active-class="active"
+      >
+        <img :src="item.icon" class="icon" />
+        <img :src="item.select_icon" class="select-icon" />
+        <span>{{item.text}}</span>
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab-main',
+  data () {
+    return {
+      tabs: [
+        {
+          path: '/home',
+          text: '首页',
+          icon: require('@/assets/image/home.png'),
+          select_icon: require('@/assets/image/home-select.png')
+        },
+        {
+          path: '/classify',
+          text: '分类',
+          icon: require('@/assets/image/classify.png'),
+          select_icon: require('@/assets/image/classify-select.png')
+        },
+        {
+          path: '/car',
+          text: '购物车',
+          icon: require('@/assets/image/car.png'),
+          select_icon: require('@/assets/image/car-select.png')
+        },
+        {
+          path: '/mine',
+          text: '我的',
+          icon: require('@/assets/image/mine.png'),
+          select_icon: require('@/assets/image/mine-select.png')
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  min-height: 100vh;
+  background: #F8F8F8;
+}
+.footer-nav {
+  @include flex(row, center, center);
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background: #fff;
+  a {
+    @include flex(column, center, center);
+    flex: 1;
+    font-size: 14px;
+    padding-bottom: 3px;
+    padding-top: 7px;
+    color: $color-grey;
+    .icon {
+      @include wh(52px, 52px);
+    }
+    .select-icon {
+      display: none;
+    }
+    &.active {
+      color: $color-primary;
+      .icon {
+        @include wh(52px, 52px);
+        display: none;
+      }
+      .select-icon {
+        @include wh(52px, 52px);
+        display: block;
+      }
+    }
+  }
+}
+</style>
